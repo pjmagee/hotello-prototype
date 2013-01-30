6a78d55de8811ded24a15e25363917f290ddffed
Patrick Magee <patrick.magee@live.co.uk>
2013-01-30 03:52:15 +0000

    NuGet Package Cleaning

--- Hotello.Common/EnumerableExtensions.cs
+++ Hotello.Common/EnumerableExtensions.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 
-namespace Hotello.UI.Web.Helpers
+namespace Hotello.Common
 {
     public static class EnumerableExtensions
     {
--- Hotello.Services.GeoIp.Tests/GeoIpTests.cs
+++ Hotello.Services.GeoIp.Tests/GeoIpTests.cs
@@ -1,10 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net.Http;
-using System.Text;
-using System.Threading.Tasks;
-using System.Web;
 using NUnit.Framework;
 
 namespace Hotello.Services.GeoIp.Tests
